08a4c269de9e51bcb2f1924bbdaa62d97e2f1344
csquares <36802782+csquares@users.noreply.github.com>
2018-04-29 13:05:32 -0500

    Country pull
    NA

---
+++ countries_process.php
@@ -0,0 +1,20 @@
+
+
+<?php
+$con=mysqli_connect("localhost","root","","squarebox");
+
+// check connection
+
+if (mysqli_connect_errno($con)) {
+	echo "Failed to connect to MySQL".mysqli_connect_error();
+}
+
+ $sql="INSERT INTO countries (country_id,country_name,region_id)
+ VALUES('$_POST[country_id_input]','$_POST[country_name_input]','$_POST[region_id_input]')";
+
+ if (!mysqli_query($con,$sql))  { 
+  die('Error: ' . mysqli_error());  }
+
+  echo "<h1>1 Country Record Added</h1>";mysqli_close($con);
+
+  ?>
