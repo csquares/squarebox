ce415acf77de8801201e485c6aad4ad0cd0e3a77
csquares <cuadros6000@gmail.com>
2021-02-23 17:16:27 -0600

    Random
    description

--- countries_process.php
+++ countries_process.php
@@ -1,9 +1,9 @@
 
-
+ 
 <?php
 $con=mysqli_connect("localhost","root","","squarebox");
 
-// check connection
+// checks connection to see if available 
 
 if (mysqli_connect_errno($con)) {
 	echo "Failed to connect to MySQL".mysqli_connect_error();
